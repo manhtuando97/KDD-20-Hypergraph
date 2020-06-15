4b0f9fb76531739dc99151ddbb7de28b09c0863e
Manh Tuan Do <42369265+manhtuando97@users.noreply.github.com>
2020-06-15 15:39:50 +0900

    Update main.java

--- Code/Decomposition/main.java
+++ Code/Decomposition/main.java
@@ -113,7 +113,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 	
 
-        String[] names = {"DAWN", "email-Eu", "tags-ask-ubuntu", "tags-math"};
+        String[] names = {};
 
         String directory = "";
      
