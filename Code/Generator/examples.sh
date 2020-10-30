e336c71f1ec7beebe8e6e3e779c8fa5c505dd9be
Manh Tuan Do <42369265+manhtuando97@users.noreply.github.com>
2020-10-30 20:14:10 +0900

    Add files via upload

---
+++ Code/Generator/examples.sh
@@ -0,0 +1,5 @@
+python hyper_preferential_attachment.py --name=DAWN --file_name=DAWN --num_nodes=3029 --simplex_per_node_directory='simplex per node' --size_distribution_directory='size distribution' --output_directory=output_directory
+
+python preferential_attachment.py --name=DAWN --file_name=DAWN --num_nodes=3029 --simplex_per_node_directory='simplex per node' --size_distribution_directory='size distribution' --output_directory=output_directory
+
+python SS.py --name=DAWN --file_name=DAWN --num_nodes=3029 --simplex_per_node_directory='simplex per node' --size_distribution_directory='size distribution' --p=0.8 --output_directory=output_directory
